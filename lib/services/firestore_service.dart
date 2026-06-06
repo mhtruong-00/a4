484b2fd80e24ab6526465cf1b5cbcda88bfd9dcd
mhtruong-00 <mhtruong@utas.edu.au>
2026-06-06 11:13:47 +1000

    Add rooms CRUD to Firestore service

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/house.dart';
+import '../models/room.dart';
 
 /// Small wrapper around Cloud Firestore so my screens don't have to talk to
 /// Firestore directly. I started with just the houses collection and will add
@@ -36,5 +37,36 @@ class FirestoreService {
   Future<void> deleteHouse(String id) {
     return _houses.doc(id).delete();
   }
+
+  CollectionReference<Map<String, dynamic>> get _rooms =>
+      _db.collection('rooms');
+
+  /// Live list of rooms for a single house. The rooms collection is top-level
+  /// and linked back to its house with the `houseId` field.
+  Stream<List<Room>> roomsStream(String houseId) {
+    return _rooms
+        .where('houseId', isEqualTo: houseId)
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs
+          .map((doc) => Room.fromMap(doc.id, doc.data()))
+          .toList();
+    });
+  }
+
+  Future<String> addRoom(Room room) async {
+    final ref = await _rooms.add(room.toMap());
+    return ref.id;
+  }
+
+  Future<void> updateRoom(Room room) {
+    return _rooms.doc(room.id).update(room.toMap());
+  }
+
+  Future<void> deleteRoom(String id) {
+    return _rooms.doc(id).delete();
+  }
 }
+
+
 
